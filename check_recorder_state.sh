796a564150a8066ccc339df0582d36d42743dba2
jayden <jayden050121@gmail.com>
2022-10-04 14:50:27 +0900

    code diet

--- check_recorder_state.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- 
-while true
-do
-  npm run recorder:state
-  sleep 5
-done
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,65 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 import dotenv from "dotenv";
 dotenv.config();
 
-import { HardhatUserConfig } from "hardhat/config";
-import "@eth-optimism/plugins/hardhat/compiler";
-
-import "hardhat-deploy";
-import "./tasks/deploy";
-import "./tasks/test";
-
-import "./tasks/Tex/ERC20/001-approve";
-import "./tasks/Tex/ERC20/002-transfer";
-import "./tasks/Tex/ERC20/003-transferFrom";
-import "./tasks/Tex/ERC20/004-permit";
-
-import "./tasks/Tex/TexFactory/001-createPair";
-import "./tasks/Tex/TexFactory/002-setFeeToSetter";
-import "./tasks/Tex/TexFactory/003-setFeeTo";
-
-import "./tasks/Tex/TexPair/001-mint";
-import "./tasks/Tex/TexPair/002-get_reserves";
-import "./tasks/Tex/TexPair/003-swap";
-import "./tasks/Tex/TexPair/004-burn";
-import "./tasks/Tex/TexPair/005-skim";
-import "./tasks/Tex/TexPair/006-sync";
-
-import "./tasks/Tex/TexRouter02/001-addLiquidity";
-import "./tasks/Tex/TexRouter02/002-addLiquidityETH";
-import "./tasks/Tex/TexRouter02/003-removeLiquidity";
-import "./tasks/Tex/TexRouter02/006-recorderState";
-import "./tasks/Tex/TexRouter02/007-approve";
-
-// import "./tasks/Hackathon/001-tex_verify";
-// import "./tasks/Hackathon/002-tex_decrypt";
-// import "./tasks/Hackathon/003-tex_add_tx_ids";
-// import "./tasks/Hackathon/004-tex_check_order";
-// import "./tasks/Hackathon/005-tex_swap";
-
-// import "./tasks/Hackathon/999-temp_1";
-// import "./tasks/Hackathon/999-temp_2";
-// import "./tasks/Hackathon/999-temp_3";
-// import "./tasks/Hackathon/999-tex_encrypt";
-
-import "./tasks/WETH9/001-deposit";
-import "./tasks/WETH9/002-withdraw";
-import "./tasks/WETH9/003-approve";
-import "./tasks/WETH9/004-transfer";
-import "./tasks/WETH9/005-transferFrom";
-
-import "./tasks/utils/accounts";
-import "./tasks/utils/001-getInitHash";
-
-//import "@nomiclabs/hardhat-etherscan";
-//import "@nomiclabs/hardhat-waffle";
-//import "@typechain/hardhat";
-
-//import "hardhat-gas-reporter";
-//import "solidity-coverage";
-
-// Check "https://hardhat.org/config/"
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
