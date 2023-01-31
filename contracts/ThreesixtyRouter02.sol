a618dcc20eac55bab7666d177d7159377540b4b2
jayden050121 <99700001+jayden050121@users.noreply.github.com>
2023-01-31 18:44:04 +0900

    Update ThreesixtyRouter02.sol

--- contracts/ThreesixtyRouter02.sol
+++ contracts/ThreesixtyRouter02.sol
@@ -225,7 +225,7 @@ contract ThreesixtyRouter02 is IThreesixtyRouter02 {
       );
       if(!recorder.isDisabledTx(txHash, swap[i].txOwner) && recorder.validate(txHash)) {
         if(swap[i].nonce == nonces[swap[i].txOwner]++) {
-          if(swap[i].functionSelector == 0x375734d9) {
+          if(swap[i].functionSelector == 0x73a2cff1) {
             (success,) = address(this).delegatecall(
               abi.encodeWithSignature(
                 "swapExactTokensForTokens(address,uint256,uint256,address[],address,uint256,bool)",
